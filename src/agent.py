ab4216702f1d8d5060b52ffc1f2dd6392097282e
CShark-4891 <152265410+CShark-4891@users.noreply.github.com>
2026-05-16 10:19:45 +0200

    integrated openrouter with template

--- src/agent.py
+++ src/agent.py
@@ -3,12 +3,14 @@ from a2a.types import Message, TaskState, Part, TextPart
 from a2a.utils import get_message_text, new_agent_text_message
 
 from messenger import Messenger
+from agents.openrouter import OpenRouterAgent
 
 
 class Agent:
     def __init__(self):
         self.messenger = Messenger()
-        # Initialize other state here
+        self._backend = OpenRouterAgent()
+
 
     async def run(self, message: Message, updater: TaskUpdater) -> None:
         """Implement your agent logic here.
@@ -20,13 +22,20 @@ class Agent:
         Use self.messenger.talk_to_agent(message, url) to call other agents.
         """
         input_text = get_message_text(message)
+        print(f"Agent received message: {input_text}")
 
         # Replace this example code with your agent logic
 
-        await updater.update_status(
-            TaskState.working, new_agent_text_message("Thinking...")
-        )
-        await updater.add_artifact(
-            parts=[Part(root=TextPart(text=input_text))],
-            name="Echo",
-        )
+        # delegate from interface to backend agent implementation 
+        # (e.g. OpenRouterAgent)
+        await self._backend.run(message, updater)
+
+        # await updater.update_status(
+        #     TaskState.working, new_agent_text_message("Thinking...")
+        # )
+
+        # await updater.add_artifact(
+        #     parts=[Part(root=TextPart(text=input_text))],
+        #     name="Echo",
+        # )
+
